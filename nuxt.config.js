f5eec905e22b5e48c2ecbe44a4cdcfd165a99f46
Jordan Bibla <jbibla@gmail.com>
2020-10-28 09:22:32 -0400

    revert big flow changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,4 +52,13 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
+
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.push({
+        path: '/',
+        redirect: `/validators`,
+      })
+    },
+  },
 }
