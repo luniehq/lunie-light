c85272a84de0aec665445603bb0bbcd90300f327
Jordan Bibla <jbibla@gmail.com>
2020-10-29 11:18:45 -0400

    Update signing/signer.js
    Co-authored-by: Fabian <frznhope@gmail.com>

--- signing/signer.js
+++ signing/signer.js
@@ -14,7 +14,7 @@ export async function getSigner(signingType, { address, password, network }) {
   // }
 
   throw new Error(
-    `Lunie doesn't support signing via ${signingType} for this network type`
+    `Signing via ${signingType} is not supported`
   )
 }
 
