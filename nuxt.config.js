bd3274648b8a607022a8ffb67e8d2afb5da390b7
Jordan Bibla <jbibla@gmail.com>
2020-10-29 08:48:01 -0400

    add a home page

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,13 +55,4 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
-
-  router: {
-    extendRoutes(routes, resolve) {
-      routes.push({
-        path: '/',
-        redirect: `/validators`,
-      })
-    },
-  },
 }
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>Home!</p>
+</template>
+
+<script>
+export default {
+  name: `home`,
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 40px;
+  text-align: center;
+  padding: 10rem;
+}
+</style>
