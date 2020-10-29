45973203eff60e47f6efbda19b1df30d9ef23e63
Jordan Bibla <jbibla@gmail.com>
2020-10-29 08:24:19 -0400

    adding language, description, and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,10 +6,13 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'Lunie 3',
+    htmlAttrs: {
+      lang: 'en'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Lunie 3 is a simple forkable wallet and staking interface for proof-of-stake blockchains' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
