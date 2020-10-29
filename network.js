90a1a9910987121285081c276d3114a017d19901
Bitcoinera <anagjordano@gmail.com>
2020-10-29 12:42:53 +0100

    prepare for terra

--- network.js
+++ network.js
@@ -1,18 +1,38 @@
 export default {
-  id: 'cosmos-mainnet',
-  chain_id: 'cosmos-hub-3', // TODO get from chain?
-  name: 'Cosmos',
-  api_url: 'https://lcd.nylira.net',
-  stakingDenom: 'ATOM',
+  id: 'terra-testnet',
+  chain_id: 'tequila-0004', // TODO get from chain?
+  name: 'Terra Testnet',
+  api_url: 'https://tequila-lcd.terra.dev',
+  stakingDenom: 'LUNA',
   coinLookup: [
     {
-      viewDenom: 'ATOM',
-      chainDenom: 'uatom',
+      viewDenom: 'LUNA',
+      chainDenom: 'uluna',
+      chainToViewConversionFactor: 1e-6,
+    },
+    {
+      viewDenom: 'KRT',
+      chainDenom: 'ukrw',
+      chainToViewConversionFactor: 1e-6,
+    },
+    {
+      viewDenom: 'MNT',
+      chainDenom: 'umnt',
+      chainToViewConversionFactor: 1e-6,
+    },
+    {
+      viewDenom: 'UST',
+      chainDenom: 'uusd',
+      chainToViewConversionFactor: 1e-6,
+    },
+    {
+      viewDenom: 'SDT',
+      chainDenom: 'usdr',
       chainToViewConversionFactor: 1e-6,
     },
   ],
   network_type: 'cosmos',
-  address_prefix: 'cosmos',
+  address_prefix: 'terra',
   HDPath: `m/44'/118'/0'/0/0`,
   curve: 'ed25519',
   lockUpPeriod: `21 days`,
