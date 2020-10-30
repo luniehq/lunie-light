493a75821d116a9c80c29c7ca302e112e9667411
Fabian Weber <frznhope@gmail.com>
2020-10-30 18:42:44 -0500

    switch to a dictionary and base on hash

--- common/keybase.js
+++ common/keybase.js
@@ -1,96 +1,82 @@
-async function loadKeybaseImages(validators) {
-  const updatedValidators = await Promise.all(
-    validators
-      .filter(({ identity }) => !!identity)
-      .map(async ({ operatorAddress, identity: keybaseHash }) => {
-        const query = `https://keybase.io/_/api/1.0/user/user_search.json?q=${keybaseHash}&num_wanted=1`
-        try {
-          const result = await fetch(query).then((res) => res.json())
-          if (result.list.length > 0) {
-            return {
-              operatorAddress,
-              picture: result.list[0].keybase.picture_url
-                ? result.list[0].keybase.picture_url.replace(
-                    'http://',
-                    'https://'
-                  )
-                : undefined,
-              updated: new Date(),
-            }
-          }
+import { keyBy, uniqBy } from 'lodash'
+
+async function queryKeybaseImages(keybaseImageRecords) {
+  const updatedKeybaseImages = await Promise.all(
+    keybaseImageRecords.map(async ({ keybaseHash }) => {
+      const query = `https://keybase.io/_/api/1.0/user/user_search.json?q=${keybaseHash}&num_wanted=1`
+      try {
+        const result = await fetch(query).then((res) => res.json())
+        if (result.list.length > 0) {
           return {
-            operatorAddress,
+            keybaseHash,
+            picture: result.list[0].keybase.picture_url
+              ? result.list[0].keybase.picture_url.replace(
+                  'http://',
+                  'https://'
+                )
+              : undefined,
             updated: new Date(),
           }
-        } catch (error) {
-          return {
-            operatorAddress,
-          }
         }
-      })
+        return {
+          keybaseHash,
+          updated: new Date(),
+        }
+      } catch (error) {
+        return {
+          keybaseHash,
+        }
+      }
+    })
   )
-  return updatedValidators
+  return keyBy(updatedKeybaseImages, 'keybaseHash')
 }
 
-function loadValidatorImages() {
-  const validatorImageRecords = JSON.parse(
-    localStorage.getItem('keybase') || '[]'
-  )
-  return validatorImageRecords
+function loadKeybaseImages() {
+  const keybaseImages = JSON.parse(localStorage.getItem('keybase') || '{}')
+  return keybaseImages
 }
 
-function saveValidatorImages(validatorImageRecords) {
-  const oldValidatorImageRecords = loadValidatorImages()
-  const mergedValidatorImageRecords = oldValidatorImageRecords
-    .filter(
-      ({ operatorAddress }) =>
-        !validatorImageRecords.find(
-          (validator) => validator.operatorAddress === operatorAddress
-        )
-    )
-    .concat(validatorImageRecords)
-  localStorage.setItem('keybase', JSON.stringify(mergedValidatorImageRecords))
+function saveKeybaseImages(keybaseImages) {
+  const oldKeybaseImages = loadKeybaseImages()
+  const mergedKeybaseImages = {
+    ...oldKeybaseImages,
+    ...keybaseImages,
+  }
+  localStorage.setItem('keybase', JSON.stringify(mergedKeybaseImages))
 }
 
 // get not yet loaded validators and outdated validators
-function getUpdateableValidatorImageRecords(currentValidators) {
-  const validatorImageRecords = loadValidatorImages()
-  const updateableImageRecords = validatorImageRecords.filter(
+function getUpdateableKeybaseEntries(currentValidators) {
+  const keybaseImages = loadKeybaseImages()
+  const updateableKeybaseImages = Object.values(keybaseImages).filter(
     ({ updated }) =>
       !updated ||
       Date.now() - new Date(updated).getTime() > 1000 * 60 * 60 * 24 * 2
   ) // update every 2 days
-  return currentValidators
-    .filter(
-      ({ operatorAddress }) =>
-        !validatorImageRecords.find(
-          (validator) => validator.operatorAddress === operatorAddress
-        )
-    )
-    .concat(
-      currentValidators.filter(({ operatorAddress }) =>
-        updateableImageRecords.find(
-          (validator) => validator.operatorAddress === operatorAddress
-        )
-      )
-    )
+  return uniqBy(
+    updateableKeybaseImages.concat(
+      // add hashes we don't know yet
+      currentValidators
+        .filter(({ identity }) => !!identity && !keybaseImages[identity])
+        .filter(({ identity }) => identity.length === 16)
+        .map(({ identity }) => ({ keybaseHash: identity }))
+    ),
+    'keybaseHash'
+  )
 }
 
 export async function updateValidatorImages(currentValidators) {
-  const updateableValidators = getUpdateableValidatorImageRecords(
-    currentValidators
-  )
-  const updatedValidatorImages = await loadKeybaseImages(updateableValidators)
-  saveValidatorImages(updatedValidatorImages)
+  const updateableKeybaseHashes = getUpdateableKeybaseEntries(currentValidators)
+  const updatedKeybaseHashes = await queryKeybaseImages(updateableKeybaseHashes)
+  saveKeybaseImages(updatedKeybaseHashes)
   return enrichtValidatorsWithPicture(currentValidators)
 }
 
 export function enrichtValidatorsWithPicture(validators) {
-  const validatorImageRecords = loadValidatorImages()
+  const validatorImageRecords = loadKeybaseImages()
   return validators.map((validator) => {
-    const imageRecord = validatorImageRecords.find(
-      ({ operatorAddress }) => validator.operatorAddress === operatorAddress
-    )
+    const imageRecord = validatorImageRecords[validator.identity]
     return {
       ...validator,
       picture: imageRecord ? imageRecord.picture : undefined,
