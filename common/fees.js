a7cd748383d64bad97a3dd407c65d6632810e18e
Bitcoinera <anagjordano@gmail.com>
2020-11-02 16:51:05 +0100

    fix fees: need to multiply by gasEstimate

--- common/fees.js
+++ common/fees.js
@@ -1,8 +1,19 @@
-import network from '~/network'
+import network from '../network'
+const BigNumber = require('bignumber.js')
 
 export default {
   ...network.fees,
   getFees(transactionType) {
-    return this[transactionType] || this.default
+    const fees = this[transactionType] || this.default
+    // convert fees amounts to view denom amounts
+    return fees.map((fee) => {
+      return {
+        ...fee,
+        fee: {
+          ...fee.fee,
+          amount: BigNumber(fee.fee.amount).times(fee.gasEstimate).toNumber(),
+        },
+      }
+    })
   },
 }
