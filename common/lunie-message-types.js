4eac34d94779efbb04d7dfd811092b49bd112633
Jordan Bibla <jbibla@gmail.com>
2020-11-04 09:42:44 -0500

    revert message type change

--- common/lunie-message-types.js
+++ common/lunie-message-types.js
@@ -1,13 +1,13 @@
 const lunieMessageTypes = {
-  SEND: `Send`,
-  STAKE: `Stake`,
-  RESTAKE: `Restake`,
-  UNSTAKE: `Unstake`,
-  VOTE: `Vote`,
-  DEPOSIT: `Deposit`,
-  CLAIM_REWARDS: `ClaimRewards`,
-  SUBMIT_PROPOSAL: `SubmitProposal`,
-  UNKNOWN: `Unknown`,
+  SEND: `SendTx`,
+  STAKE: `StakeTx`,
+  RESTAKE: `RestakeTx`,
+  UNSTAKE: `UnstakeTx`,
+  VOTE: `VoteTx`,
+  DEPOSIT: `DepositTx`,
+  CLAIM_REWARDS: `ClaimRewardsTx`,
+  SUBMIT_PROPOSAL: `SubmitProposalTx`,
+  UNKNOWN: `UnknownTx`,
 }
 
 module.exports = { lunieMessageTypes }
