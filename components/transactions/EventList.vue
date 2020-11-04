60c4951767e3819cfec7abdd58eced0cbf3a0bd2
Fabian Weber <frznhope@gmail.com>
2020-11-04 12:11:49 -0500

    fix dupe txs

--- components/transactions/EventList.vue
+++ components/transactions/EventList.vue
@@ -4,8 +4,8 @@
       <div :key="group[0].section">
         <h3>{{ group[0].section }}</h3>
         <TransactionItem
-          v-for="(item, index) in group"
-          :key="item.event.key + index"
+          v-for="item in group"
+          :key="item.event.key"
           :transaction="item.event"
         />
       </div>
