4fe96dfabd4a7ae5b64bf1e306d7287641afb0d9
Fabian Weber <frznhope@gmail.com>
2020-11-10 13:09:36 -0500

    remove depr data

--- common/akashV1-source.js
+++
@@ -1,9 +0,0 @@
-const CosmosV3API = require('./cosmosV3-source')
-
-class AkashV1API extends CosmosV3API {
-  setReducers() {
-    this.reducers = require('./akashV1-reducers')
-  }
-}
-
-module.exports = AkashV1API
