60ddc02c0e863779807fb1ebb1b017f153646db4
Fabian Weber <frznhope@gmail.com>
2020-11-11 16:56:39 -0500

    refactored  copy button

--- components/governance/ProposalHeader.vue
+++ components/governance/ProposalHeader.vue
@@ -11,15 +11,7 @@
         </div>
 
         <div class="buttons">
-          <button
-            v-clipboard:copy="currentRoute"
-            v-clipboard:success="() => onCopy()"
-            class="share-button"
-          >
-            <span v-if="copySuccess" class="copy-message">Copied!</span>
-            <i v-if="copySuccess" class="material-icons check-icon">check</i>
-            <i v-else class="material-icons">link</i>
-          </button>
+          <CopyButton :value="currentRoute" />
           <Button
             v-if="status.value === governanceStatusEnum.DEPOSITING"
             id="deposit-btn"
@@ -89,7 +81,6 @@ export default {
     },
   },
   data: () => ({
-    copySuccess: false,
     governanceStatusEnum,
   }),
   computed: {
@@ -105,14 +96,6 @@ export default {
         this.proposal.type !== `TREASURY` &&
         this.status.value === this.governanceStatusEnum.VOTING
       )
-    },
-  },
-  methods: {
-    onCopy() {
-      this.copySuccess = true
-      setTimeout(() => {
-        this.copySuccess = false
-      }, 2500)
     },
   },
 }
@@ -199,26 +182,6 @@ h2 {
   font-style: italic;
 }
 
-.share-button {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 0.5rem;
-  outline: none;
-  height: 2.25rem;
-  font-size: 14px;
-  background: transparent;
-  color: #7a88b8;
-  border: 2px solid rgb(122, 136, 184, 0.1);
-  cursor: pointer;
-  width: 3rem;
-  position: relative;
-}
-
-.share-button:hover {
-  background-color: var(--bc);
-}
-
 .icon-container {
   margin: 0 0.5rem 0 0;
 }
@@ -234,18 +197,6 @@ h2 {
 
 .outer-status {
   display: none;
-}
-
-.check-icon {
-  font-size: 18px;
-  color: var(--success);
-}
-
-.copy-message {
-  position: absolute;
-  font-size: 10px;
-  top: -1.25rem;
-  color: var(--success);
 }
 
 @media screen and (max-width: 667px) {
