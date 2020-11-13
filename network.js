bef386e0d1f5dd43b694e15d0e49f506003ffc6d
Bitcoinera <anagjordano@gmail.com>
2020-11-13 20:11:50 +0100

    switch to gaia

--- network.js
+++ network.js
@@ -1,16 +1,18 @@
+require('dotenv').config()
+
 export default {
-  id: 'cosmos-mainnet',
-  name: 'Cosmos Hub',
+  id: 'cosmos-hub-testnet',
+  name: 'Gaia Testnet',
   description:
     'Cosmos is a decentralized network of independent parallel blockchains, each powered by BFT consensus algorithms like Tendermint consensus.',
   logo: `logo.svg`,
   website: 'https://cosmos.network',
-  apiURL: 'https://lcd.nylira.net',
-  stakingDenom: 'ATOM',
+  apiURL: `https://api.allorigins.win/raw?url=${process.env.NETWORK_API_URL}`,
+  stakingDenom: 'MUON',
   coinLookup: [
     {
-      viewDenom: 'ATOM',
-      chainDenom: 'uatom',
+      viewDenom: 'MUON',
+      chainDenom: 'umuon',
       chainToViewConversionFactor: 1e-6,
     },
   ],
@@ -18,13 +20,13 @@ export default {
   validatorAddressPrefix: 'cosmosvaloper',
   validatorConsensusaddressPrefix: 'cosmosvalcons', // needed to map validators from staking queries to the validator set
   HDPath: `m/44'/118'/0'/0/0`,
-  lockUpPeriod: `21 days`,
+  lockUpPeriod: `3 days`,
   fees: {
     default: {
       gasEstimate: 350000,
       feeOptions: [
         {
-          denom: 'ATOM',
+          denom: 'MUON',
           amount: 0.001,
         },
       ],
