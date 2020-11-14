cd8f29e5e52309af89a2f5a73b350c8a8d9cbc76
Bitcoinera <anagjordano@gmail.com>
2020-11-14 12:51:20 +0100

    fix local sessionType in cookie

--- pages/recover/index.vue
+++ pages/recover/index.vue
@@ -80,7 +80,7 @@ export default {
         )
         this.$store.dispatch('signIn', {
           address: wallet.address,
-          type: 'local',
+          sessionType: 'local',
         })
         this.$router.push({
           name: 'portfolio',
