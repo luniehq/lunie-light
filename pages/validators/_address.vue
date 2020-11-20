32da05cf29a893d4d2b36009553478db63c3b503
Jordan Bibla <jbibla@gmail.com>
2020-11-19 23:56:30 -0500

    new styles for validator profile page, fixes for watcher

--- pages/validators/_address.vue
+++ pages/validators/_address.vue
@@ -3,150 +3,142 @@
     <div v-if="!validatorsLoaded">
       <Loader />
     </div>
-    <div v-else-if="validators.length && !validator">Validator Not Found</div>
-    <div v-else class="readable-width">
-      <div class="back-button-container">
-        <BackButton />
-      </div>
+    <Card
+      v-else-if="validators.length && !validator"
+      title="Validator Not Found"
+      subtitle="Please try again."
+    />
+
+    <div v-else class="validator-container">
       <div class="status-container">
         <Status :label="validator.status" />
       </div>
-      <tr class="li-validator">
-        <td class="data-table__row__info">
-          <div class="li-validator-name-row">
-            <Avatar
-              v-if="!validator.picture || validator.picture === 'null'"
-              class="li-validator-image"
-              alt="generic geometric symbol - generated avatar from address"
-              :address="validator.operatorAddress"
-            />
-            <img
-              v-else-if="validator.picture"
-              :src="validator.picture"
-              :alt="`validator logo for ` + validator.name"
-              class="li-validator-image"
-            />
-            <div class="validator-info">
-              <h3 class="li-validator-name">{{ validator.name }}</h3>
-              <div v-if="delegation && delegation.amount">
-                <h4>{{ delegation.amount | fullDecimals }}</h4>
-                <h5 v-if="rewards && rewards.length > 0">
-                  +{{ filterStakingDenomReward() | noBlanks }}
-                </h5>
-              </div>
+      <div class="validator-header">
+        <div class="identity">
+          <Avatar
+            class="image"
+            alt="validator logo"
+            :address="validator.operatorAddress"
+          />
+          <div class="validator-info">
+            <h3 class="name">{{ validator.name }}</h3>
+            <div v-if="delegation && delegation.amount">
+              <h4>{{ delegation.amount | fullDecimals }}</h4>
+              <h5 v-if="rewards && rewards.length > 0">
+                +{{ filterStakingDenomReward() | noBlanks }}
+              </h5>
             </div>
           </div>
-        </td>
-      </tr>
-
-      <div class="action-button-container">
-        <Button
-          id="delegation-btn"
-          :value="`Stake`"
-          @click.native="onDelegation"
-        />
-        <Button
-          id="undelegation-btn"
-          class="undelegation-btn"
-          :disabled="!delegation"
-          :value="`Unstake`"
-          type="secondary"
-          @click.native="onUndelegation"
-        />
+        </div>
+        <div class="action-buttons">
+          <Button :value="`Stake`" @click.native="onDelegation" />
+          <Button
+            :disabled="!delegation"
+            :value="`Unstake`"
+            type="secondary"
+            @click.native="onUndelegation"
+          />
+        </div>
       </div>
 
-      <ul class="row">
-        <li class="column">
+      <section class="validator-data">
+        <div>
           <h4>Description</h4>
           <span>{{ validator.details | noBlanks }}</span>
-        </li>
-        <li class="column">
+        </div>
+        <div>
           <h4>Website</h4>
           <span v-if="validator.website">
             <a
-              id="validator-website"
               :href="validator.website + `?ref=lunie`"
               target="_blank"
               rel="nofollow noreferrer noopener"
               >{{ validator.website }}</a
             >
           </span>
-          <span v-else id="validator-website">
+          <span v-else>
             {{ validator.website | noBlanks }}
           </span>
-        </li>
-        <li class="column">
+        </div>
+        <div>
           <h4>Validator Address</h4>
           <span>
             <Address :address="validator.operatorAddress" />
           </span>
-        </li>
-      </ul>
+        </div>
+      </section>
 
-      <ul class="row">
-        <li>
+      <section class="row">
+        <div>
           <h4>Rewards</h4>
-          <span id="page-profile__rewards">
+          <span>
             {{ validator.expectedReturns | percent }}
           </span>
-        </li>
-        <li>
-          <h4>Voting Power / Total Stake</h4>
-          <span id="page-profile__power">
-            {{ validator.votingPower | percent }} /
+          <span class="note">Approx. Annualized</span>
+        </div>
+        <div>
+          <h4>Voting Power</h4>
+          <span>
+            {{ validator.votingPower | percent }}
+          </span>
+        </div>
+      </section>
+      <section class="row">
+        <div>
+          <h4>Total Stake</h4>
+          <span>
             {{ validator.tokens | shortDecimals }}
           </span>
-        </li>
-        <li>
-          <h4>Self Stake</h4>
-          <span id="page-profile__self-bond">
-            {{ selfStake | shortDecimals }} /
-            {{ (selfStake / validator.tokens || 0) | percent }}
-          </span>
-        </li>
-        <li>
+          <span class="note">
+            (self stake: {{ selfStake | shortDecimals }})</span
+          >
+        </div>
+        <div>
+          <h4>Number of Stakers</h4>
+          <span>{{ validatorDelegations.length }}</span>
+        </div>
+      </section>
+      <section class="row">
+        <div>
+          <h4>Uptime</h4>
+          <span>{{ isBlankField(validator.uptimePercentage, percent) }}</span>
+        </div>
+        <div>
           <h4>Validator Since</h4>
           <span>Block #{{ validator.startHeight || 0 }}</span>
-        </li>
-        <li>
-          <h4>Uptime</h4>
-          <span id="page-profile__uptime">{{
-            isBlankField(validator.uptimePercentage, percent)
-          }}</span>
-        </li>
-        <li>
+        </div>
+      </section>
+      <section class="row">
+        <div>
           <h4>Current Commission Rate</h4>
           <span>{{ isBlankField(validator.commission, percent) }}</span>
-        </li>
-        <li>
+        </div>
+        <div>
           <h4>Max Commission Rate</h4>
           <span>{{ isBlankField(validator.maxCommission, percent) }}</span>
-        </li>
-        <li>
+        </div>
+      </section>
+      <section class="row">
+        <div>
           <h4>Max Daily Commission Change</h4>
           <span>{{
             isBlankField(validator.maxChangeCommission, percent)
           }}</span>
-        </li>
-        <li>
+        </div>
+        <div>
           <h4>Last Commission Change</h4>
           <span>{{
             isBlankField(validator.commissionUpdateTime, fromNow)
           }}</span>
-        </li>
-        <li>
-          <h4>Number of Stakers</h4>
-          <span>{{ validatorDelegations.length }}</span>
-        </li>
-      </ul>
-
-      <LazyStakeModal ref="stakeModal" :target-validator="validator" />
-      <LazyUnstakeModal
-        ref="unstakeModal"
-        :source-validator="validator"
-        :is-unnomination="true"
-      />
+        </div>
+      </section>
     </div>
+    <LazyStakeModal ref="stakeModal" :target-validator="validator" />
+    <LazyUnstakeModal
+      ref="unstakeModal"
+      :source-validator="validator"
+      :is-unnomination="true"
+    />
   </div>
 </template>
 
@@ -197,11 +189,14 @@ export default {
     },
   },
   watch: {
-    validator(validator) {
-      if (validator) {
-        this.getSelfStake()
-        this.getValidatorDelegations()
-      }
+    validator: {
+      immediate: true,
+      handler(validator) {
+        if (validator) {
+          this.getValidatorDelegations()
+          this.getSelfStake()
+        }
+      },
     },
   },
   methods: {
@@ -246,26 +241,42 @@ export default {
 }
 </script>
 <style scoped>
-span {
-  font-size: var(--text-xs);
-  line-height: normal;
+.validator-container {
+  margin: 2rem auto;
+  padding: 1.5rem 2rem;
+  box-shadow: 0 0 3px 0 var(--gray-400);
+  border-radius: var(--border-radius);
+  background: var(--white);
+  max-width: 1024px;
 }
 
-.li-validator {
+section {
+  box-shadow: 0 0 3px 0 var(--gray-400);
+  border-radius: var(--border-radius);
+  background: var(--white);
+  margin: 1rem 0;
+}
+
+.validator-header {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 1rem;
-  border-bottom: 1px solid var(--bc-dim);
+  padding: 1rem 1rem 2rem;
 }
 
-.li-validator-image {
-  border-radius: 50%;
+.validator-header .validator-image {
   height: 4rem;
+  min-height: 4rem;
+  width: 4rem;
   min-width: 4rem;
 }
 
-.li-validator-name {
+.identity {
+  display: flex;
+  align-items: center;
+}
+
+.name {
   color: var(--bright);
   font-size: var(--text-3xl);
   line-height: 2rem;
@@ -277,7 +288,7 @@ span {
 .validator-info {
   display: flex;
   flex-direction: column;
-  padding-left: 1rem;
+  padding: 0 1rem;
   text-overflow: ellipsis;
 }
 
@@ -288,49 +299,23 @@ h4 {
   font-weight: 500;
 }
 
-.li-validator h4,
-.li-validator h5 {
+.h4,
+.h5 {
   font-size: var(--text-xs);
   display: inline-block;
 }
 
-.li-validator h5 {
+.h5 {
   padding-left: 0.5rem;
   color: var(--success);
 }
 
-.li-validator > .data-table__row__info {
-  display: block;
-  width: 100%;
-}
-
-.li-validator .li-validator-name-row {
+.action-buttons {
   display: flex;
   align-items: center;
 }
 
-.back-button-container {
-  display: flex;
-  padding: 0 1rem;
-}
-
-.button-container {
-  justify-content: space-between;
-}
-
-.button-container,
-.action-button-container {
-  display: flex;
-  align-items: center;
-  padding: 0.5rem 1rem;
-}
-
-.action-button-container {
-  border-bottom: 1px solid var(--bc-dim);
-}
-
-.action-button-container button:first-child,
-.button-container button:first-child {
+.action-buttons button:first-child {
   margin-right: 0.5rem;
 }
 
@@ -338,79 +323,49 @@ h4 {
   padding: 1rem 1rem 0;
 }
 
-.page {
-  position: relative;
+section div {
   width: 100%;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  padding: 1rem 0 0;
+  padding: 1rem;
 }
 
-.readable-width {
-  max-width: 800px;
-  margin: 1rem auto;
-}
-
-.page.dark-background {
-  background: var(--app-fg);
-}
-
-.column {
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  align-items: normal;
-  width: 100%;
+section:not(.row) div {
+  border-bottom: 1px solid var(--gray-200);
 }
 
 .row {
   display: flex;
-  flex-direction: column;
-  width: 100%;
-  padding: 2rem 0 1rem;
+  flex-direction: row;
 }
 
-.page-profile__section {
-  margin-bottom: 1rem;
-}
-
-.page-profile__section-title {
-  margin: 0 0 0.25rem 1rem;
-  color: var(--txt);
-  font-size: var(--text-sm);
-  font-weight: 500;
-}
-
-li {
-  width: 100%;
-  padding: 1rem;
-  border-bottom: 1px solid var(--gray-200);
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-li:last-child {
+section div:last-child {
   border-bottom: none;
 }
 
-.row span {
-  color: var(--bright);
-  font-size: var(--text-sm);
-  font-weight: 400;
+.copyable-address {
+  padding: 0;
+  font-size: var(--text-base);
 }
 
-@media screen and (max-width: 667px) {
-  .button-container {
-    width: 100%;
-    padding: 0 1rem;
+.note {
+  font-size: var(--text-xs);
+  color: var(--dim);
+  font-style: italic;
+}
+
+@media screen and (max-width: 767px) {
+  .status-container {
+    display: flex;
+    justify-content: center;
   }
 
-  .button-container button,
-  .action-button-container button {
-    width: 50%;
+  .validator-info {
+    padding-top: 1.5rem;
+  }
+
+  .validator-header,
+  .identity {
+    flex-direction: column;
+    padding: 1rem 0;
   }
 }
 </style>
