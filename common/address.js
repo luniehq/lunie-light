5bd8eab714c8a612a87ba91524dec58290c08627
Jordan Bibla <jbibla@gmail.com>
2020-11-23 11:26:04 -0500

    made formatAddress simpler and cosmos zone agnostic

--- common/address.js
+++ common/address.js
@@ -22,29 +22,11 @@ const pubkeyToAddress = (cosmosvalconspub, validatorConsensusBech32Prefix) => {
   return hexToValidatorAddress(hexAddress, validatorConsensusBech32Prefix)
 }
 
-const getAddressType = (address) => {
-  if (address.startsWith('0x')) return 'ethereum'
-  try {
-    bech32.decode(address)
-    return 'cosmos'
-  } catch (error) {
-    // ignore error
-  }
-  return 'any'
-}
-
 const formatAddress = (address, length = 4) => {
   if (!address) {
     return `Address Not Found`
   }
-  switch (getAddressType(address)) {
-    case 'cosmos':
-      return address.split(`1`)[0] + `…` + address.slice(-1 * length)
-    case 'ethereum':
-      return address.slice(0, 2 + length) + `…` + address.slice(-1 * length)
-    case 'any':
-      return address.slice(0, length) + `…` + address.slice(-1 * length)
-  }
+  return address.split(`1`)[0] + `…` + address.slice(-1 * length)
 }
 
 module.exports = {
