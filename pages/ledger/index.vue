2a4f988f5836cfa2bf16be009905b085db0b028c
Bitcoinera <anagjordano@gmail.com>
2020-11-24 19:28:02 +0100

    add linux instructions and copyable link styles

--- pages/ledger/index.vue
+++ pages/ledger/index.vue
@@ -9,7 +9,7 @@
         @click.native="connect"
       />
     </div>
-    <div v-if="isWindows && !hasHIDEnabled">
+    <div v-if="isWindows && !hasHIDEnabled && !error">
       Due to recent Ledger updates, using a Ledger on Windows now requires
       "Experimental Web Platform features" to be enabled.
       <div v-if="isChrome">
@@ -36,10 +36,37 @@
         <br />
       </div>
     </div>
+    <div v-else-if="isLinux && !error">
+      Since we switched to WebUSB Linux users may experience connection issues
+      with their devices.
+      <br />
+      <br />
+      Please visit the following site to learn more about how to fix them:
+      <div
+        v-clipboard:copy="linuxLedgerConnectionLink"
+        v-clipboard:success="() => onCopy()"
+        class="copy-feature-link"
+      >
+        {{ linuxLedgerConnectionLink }}
+        <i
+          class="material-icons notranslate copied"
+          :class="{ active: copySuccess }"
+        >
+          check
+        </i>
+      </div>
+      <br />
+      <br />
+    </div>
     <div v-if="error" class="error-container">
       <p>There was an error connecting to the Ledger Nano:<br /></p>
       <p class="error">
         {{ error }}
+      </p>
+    </div>
+    <div v-else class="session-main">
+      <p>
+        Please use Chrome or Brave. Ledger is not supported in this browser.
       </p>
     </div>
   </div>
@@ -54,6 +81,7 @@ export default {
     copySuccess: false,
     navigator: window.navigator,
     hidFeatureLink: `chrome://flags/#enable-experimental-web-platform-features`,
+    linuxLedgerConnectionLink: `https://support.ledger.com/hc/en-us/articles/360019301813-Fix-USB-issues`,
   }),
   computed: {
     ...mapState('ledger', [`accounts`, `error`, `loading`]),
@@ -63,6 +91,9 @@ export default {
     isChrome() {
       const ua = navigator.userAgent.toLowerCase()
       return /chrome|crios/.test(ua) && !/edge|opr\//.test(ua)
+    },
+    isLinux() {
+      return this.navigator.platform.includes('Lin')
     },
     hasHIDEnabled() {
       return !!this.navigator.hid
@@ -108,5 +139,30 @@ export default {
 .session-main {
   display: flex;
   justify-content: center;
+  margin-bottom: 1rem;
+}
+
+.copy-feature-link {
+  display: initial;
+  font-size: 0.8rem;
+  cursor: pointer;
+  margin-bottom: 0.2rem;
+  color: var(--link);
+}
+
+.copy-feature-link .material-icons {
+  font-size: 12px;
+}
+
+.copy-feature-link .copied {
+  padding-bottom: 2px;
+  padding-right: 0;
+  transition: opacity 500ms ease;
+  color: var(--success);
+  opacity: 0;
+}
+
+.copy-feature-link .copied.active {
+  opacity: 1;
 }
 </style>
