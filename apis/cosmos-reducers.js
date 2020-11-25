7e67d7f599546e41e1333ac4754ec5635c420ceb
Bitcoinera <anagjordano@gmail.com>
2020-11-25 16:49:58 +0100

    fix amount not displaying

--- apis/cosmos-reducers.js
+++ apis/cosmos-reducers.js
@@ -363,7 +363,7 @@ export function sendDetailsReducer(message) {
   return {
     from: [message.from_address],
     to: [message.to_address],
-    amount: message.amount.map(coinReducer),
+    amounts: message.amount.map(coinReducer),
   }
 }
 
