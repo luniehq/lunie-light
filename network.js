c5d643ad688e58704787d144cb43a53568a9fb33
Bitcoinera <anagjordano@gmail.com>
2020-11-25 16:51:43 +0100

    switch back to stargate

--- network.js
+++ network.js
@@ -1,121 +1,37 @@
 export default {
-  id: 'emoney-2',
-  name: 'e-Money',
+  id: 'cosmos-hub-testnet',
+  name: 'Cosmos Stargate',
   description:
-    'e-Money is the leading issuer of interest-bearing, currency-backed stablecoins for the Internet of Money.',
+    'Cosmos is a network of independent parallel blockchains, powered by BFT consensus algorithms like Tendermint.',
   logo: `logo.svg`,
-  website: 'https://e-money.com',
-  rpcURL: 'https://emoney.validator.network/',
-  apiURL: 'https://emoney.validator.network/light',
-  stakingDenom: 'NGM',
+  website: 'https://cosmos.network',
+  apiURL: 'http://localhost:8010/proxy', // use `npx lcp --proxyUrl http://34.123.30.100:1317`
+  rpcURL: 'ws://34.123.30.100:26657',
+  stakingDenom: 'MUON',
   coinLookup: [
     {
-      viewDenom: 'NGM',
-      chainDenom: 'ungm',
+      viewDenom: 'MUON',
+      chainDenom: 'umuon',
       chainToViewConversionFactor: 1e-6,
-      icon: `network-icons/e-money.png`,
-    },
-    {
-      viewDenom: 'eEUR',
-      chainDenom: 'eeur',
-      chainToViewConversionFactor: 1e-6,
-      icon: `network-icons/e-money.png`,
-    },
-    {
-      viewDenom: 'eCHF',
-      chainDenom: 'echf',
-      chainToViewConversionFactor: 1e-6,
-      icon: `network-icons/e-money.png`,
-    },
-    {
-      viewDenom: 'eSEK',
-      chainDenom: 'esek',
-      chainToViewConversionFactor: 1e-6,
-      icon: `network-icons/e-money.png`,
-    },
-    {
-      viewDenom: 'eNOK',
-      chainDenom: 'enok',
-      chainToViewConversionFactor: 1e-6,
-      icon: `network-icons/e-money.png`,
-    },
-    {
-      viewDenom: 'eDKK',
-      chainDenom: 'edkk',
-      chainToViewConversionFactor: 1e-6,
-      icon: `network-icons/e-money.png`,
-    },
-    {
-      viewDenom: 'ATOM',
-      chainDenom: 'uatom',
-      chainToViewConversionFactor: 1e-6,
-      icon: `network-icons/cosmos.png`,
+      icon: `currencies/muon.png`,
     },
   ],
-  addressPrefix: 'emoney',
-  validatorAddressPrefix: 'emoneyvaloper',
-  validatorConsensusaddressPrefix: 'emoneyvalcons', // needed to map validators from staking queries to the validator set
+  addressPrefix: 'cosmos',
+  validatorAddressPrefix: 'cosmosvaloper',
+  validatorConsensusaddressPrefix: 'cosmosvalcons', // needed to map validators from staking queries to the validator set
   HDPath: `m/44'/118'/0'/0/0`,
   lockUpPeriod: `3 days`,
   fees: {
     default: {
-      gasEstimate: 200000,
+      gasEstimate: 350000,
       feeOptions: [
         {
-          denom: 'NGM',
-          amount: 0.2,
-        },
-        {
-          denom: 'eEUR',
-          amount: 0.05,
-        },
-        {
-          denom: 'eCHF',
-          amount: 0.053,
-        },
-        {
-          denom: 'eDKK',
-          amount: 0.37,
-        },
-        {
-          denom: 'eSEK',
-          amount: 0.55,
-        },
-        {
-          denom: 'eNOK',
-          amount: 0.61,
-        },
-      ],
-    },
-    SendTx: {
-      gasEstimate: 100000,
-      feeOptions: [
-        {
-          denom: 'NGM',
-          amount: 0.1,
-        },
-        {
-          denom: 'eEUR',
-          amount: 0.05,
-        },
-        {
-          denom: 'eCHF',
-          amount: 0.053,
-        },
-        {
-          denom: 'eDKK',
-          amount: 0.37,
-        },
-        {
-          denom: 'eSEK',
-          amount: 0.55,
-        },
-        {
-          denom: 'eNOK',
-          amount: 0.61,
+          denom: 'MUON',
+          amount: 0.001,
         },
       ],
     },
   },
-  localSigning: true,
+  icon: `https://lunie.fra1.digitaloceanspaces.com/network-icons/cosmos.png`,
+  localSigning: true, // this is only to be used as a developer tool - never deployed in production or for mainnet chains
 }
